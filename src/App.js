9f54758fd3d64078f664f90fd04a27e9dfa497e3
TMGabella <tmgabella@gmail.com>
2024-01-16 15:48:03 +0700

    sdsd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialogs';
+import Music from './components/Music/Music';
+import News from './components/News/News';
+import Settings from './components/Settings/Settings';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+ 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	return (
+		<BrowserRouter>
+			<div className='app-wrapper'>
+				<Header />
+				<Navbar />
+				<div className='app-wrapper-content'>
+					<Routes>
+						<Route path='/profile' Component={Profile} />
+						<Route path='/dialogs' Component={Dialogs} />
+						<Route path='/music' Component={Music} />
+						<Route path='/news' Component={News} />
+						<Route path='/settings' Component={Settings} />
+					</Routes>
+				</div>
+			</div>
+		</BrowserRouter>
+	);
 }
+
+
+
 
 export default App;
